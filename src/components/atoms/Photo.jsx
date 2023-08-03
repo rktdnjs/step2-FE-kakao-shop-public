7cf5c08d6d6a03c74749aacaa1f83d99ed5864b3
rktdnjs <rktdnjs@naver.com>
2023-08-03 16:33:39 +0900

    Photo src URL 수정

--- src/components/atoms/Photo.jsx
+++ src/components/atoms/Photo.jsx
@@ -1,14 +1,16 @@
 import { styled } from "styled-components";
 import "../../styles/Photo.css"
 
+const staticServerUri = process.env.REACT_APP_PATH || "";
+
 const Photo = ({className, src, alt}) => {
     return (
         <>  
             {/* picture : 검색엔진최적화 가능 */}
             {/* 소스와 이미지를 둘다 가져와서 최적화된 것을 보여준다. */}
             <picture className={className}>
-                <source srcSet={process.env.REACT_APP_API_URL + src} />
-                <ProductImg src={process.env.REACT_APP_API_URL + src} alt={alt} />
+                <source srcSet={staticServerUri + src} />
+                <ProductImg src={staticServerUri + src} alt={alt} />
             </picture>
         </>
     );
